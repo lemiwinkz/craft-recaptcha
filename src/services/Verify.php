ff23f13caa3d58574f927df4b1383f48b7506479
Giel Tettelaar <water.giel@gmail.com>
2018-03-17 22:38:32 +0100

    Update Verify.php

--- src/services/Verify.php
+++ src/services/Verify.php
@@ -15,41 +15,10 @@ use aberkie\recaptcha\Recaptcha;
 use Craft;
 use craft\base\Component;
 
-/**
- * Verify Service
- *
- * All of your plugin’s business logic should go in services, including saving data,
- * retrieving data, etc. They provide APIs that your controllers, template variables,
- * and other plugins can interact with.
- *
- * https://craftcms.com/docs/plugins/services
- *
- * @author    Aaron Berkowitz
- * @package   Recaptcha
- * @since     2.0.0
- */
 class Verify extends Component
 {
     // Public Methods
     // =========================================================================
 
-    /**
-     * This function can literally be anything you want, and you can have as many service
-     * functions as you want
-     *
-     * From any other plugin file, call it like this:
-     *
-     *     Recaptcha::$plugin->verify->exampleService()
-     *
-     * @return mixed
-     */
-    public function exampleService()
-    {
-        $result = 'something';
-        // Check our Plugin's settings for `someAttribute`
-        if (Recaptcha::$plugin->getSettings()->someAttribute) {
-        }
-
-        return $result;
-    }
+    // TODO : VERIFY SERVICE
 }
