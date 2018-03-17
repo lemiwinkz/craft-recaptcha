dab98db99bfaec613d02212bf9b81dbd0b167850
Giel Tettelaar <water.giel@gmail.com>
2018-03-17 22:39:09 +0100

    Update ServiceController.php

--- src/controllers/ServiceController.php
+++ src/controllers/ServiceController.php
@@ -15,65 +15,12 @@ use aberkie\recaptcha\Recaptcha;
 use Craft;
 use craft\web\Controller;
 
-/**
- * Service Controller
- *
- * Generally speaking, controllers are the middlemen between the front end of
- * the CP/website and your plugin’s services. They contain action methods which
- * handle individual tasks.
- *
- * A common pattern used throughout Craft involves a controller action gathering
- * post data, saving it on a model, passing the model off to a service, and then
- * responding to the request appropriately depending on the service method’s response.
- *
- * Action methods begin with the prefix “action”, followed by a description of what
- * the method does (for example, actionSaveIngredient()).
- *
- * https://craftcms.com/docs/plugins/controllers
- *
- * @author    Aaron Berkowitz
- * @package   Recaptcha
- * @since     2.0.0
- */
+
 class ServiceController extends Controller
 {
-
-    // Protected Properties
-    // =========================================================================
-
-    /**
-     * @var    bool|array Allows anonymous access to this controller's actions.
-     *         The actions must be in 'kebab-case'
-     * @access protected
-     */
-    protected $allowAnonymous = ['index', 'do-something'];
 
     // Public Methods
     // =========================================================================
 
-    /**
-     * Handle a request going to our plugin's index action URL,
-     * e.g.: actions/recaptcha/service
-     *
-     * @return mixed
-     */
-    public function actionIndex()
-    {
-        $result = 'Welcome to the ServiceController actionIndex() method';
-
-        return $result;
-    }
-
-    /**
-     * Handle a request going to our plugin's actionDoSomething URL,
-     * e.g.: actions/recaptcha/service/do-something
-     *
-     * @return mixed
-     */
-    public function actionDoSomething()
-    {
-        $result = 'Welcome to the ServiceController actionDoSomething() method';
-
-        return $result;
-    }
+    // TODO : PORT C2 Controller to here
 }
